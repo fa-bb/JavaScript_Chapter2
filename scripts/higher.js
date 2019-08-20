2193e427dd207adcbede183b2342d8c92f9dd9b0
hiro.h <h.hashimoto.web@gmail.com>
2019-08-20 14:47:58 +0900

    P222

---
+++ scripts/higher.js
@@ -0,0 +1,16 @@
+function arrayWalk(data,f){
+  for(var key in data){
+    f(data[key],key);
+  }
+}
+
+//fがshowElementになる
+
+function showElement(value,key){
+  console.log(key+':'+value);
+}
+
+var ary = [1,2,4,8,16];
+arrayWalk(ary,showElement);
+
+
