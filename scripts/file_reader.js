44e951c9b60bb54d6d8948777a20bad9b47ff1aa
hiro.h <h.hashimoto.web@gmail.com>
2019-09-03 11:49:27 +0900

    6.4

---
+++ scripts/file_reader.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded',function(){
+  console.log('file_reader');
+  document.getElementById("file").addEventListener('change',function(e){
+    var input = document.getElementById("file").files[0];
+    var reader = new FileReader();
+    reader.addEventListener('load',function(){
+      document.getElementById("result").textContent = reader.result;
+    },true);
+
+    reader.readAsText(input,'UTF-8');
+  },true);
+});
