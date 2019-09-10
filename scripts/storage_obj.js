ce6060db4b64b5f37c47c1a5bba1f1bfaa2d486b
hiro.h <h.hashimoto.web@gmail.com>
2019-09-10 14:06:15 +0900

    7.3

---
+++ scripts/storage_obj.js
@@ -0,0 +1,8 @@
+var storage = localStorage;
+var apple = {name:'りんご',price:150,made:'青森'};
+storage.setItem('apple',JSON.stringify(apple));
+var data = JSON.parse(storage.getItem('apple'));
+console.log(data.name);
+console.log(data.price);
+console.log(data.made);
+
